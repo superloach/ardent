312ceb71ab9a96c1618fca8723aa0b36a61e09e7
Zachary Burkett <zburkett@splitcubestudios.com>
2020-10-04 17:07:59 -0400

    Cleanup tilemap

--- internal/common/tilemap.go
+++ internal/common/tilemap.go
@@ -13,14 +13,6 @@ type Tilemap struct {
 }
 
 func (t *Tilemap) isoToIndex(x, y float64) (int, int) {
-	/*
-		i := y*4/float64(t.Width) + 1
-		j := (i*float64(t.Width) + 2*x) / float64(2*t.Width)
-
-		//	return int(math.Round(i-j)) + 1, int(math.Round(j))
-		return int(math.Round(j)), int(math.Round(i-j)) + 1
-	*/
-
 	ix := int(math.Ceil((x/float64(t.Width/2) + y/float64(t.Width/4)) / 2))
 	iy := int(math.Ceil((y/float64(t.Width/4) - x/float64(t.Width/2)) / 2))
 
@@ -28,15 +20,6 @@ func (t *Tilemap) isoToIndex(x, y float64) (int, int) {
 }
 
 func (t *Tilemap) indexToIso(i, j int) (float64, float64) {
-	/*
-		i += j
-
-		y := i * t.Width / 4
-		x := ((j - i/2) * t.Width) - (t.Width * (i % 2) / 2)
-
-		return float64(x), float64(y)
-	*/
-
 	x := (i - j) * (t.Width / 2)
 	y := (i + j) * (t.Width / 4)
 
