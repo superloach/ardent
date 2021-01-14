f5bf9cb493de4e56ae8b1a4dead27de44e2be033
Terra Brown <superloach42@gmail.com>
2021-01-13 19:03:03 -0500

    add comment about layoutfill

--- engine/layout.go
+++ engine/layout.go
@@ -5,12 +5,13 @@ package engine
 // Basic implementations include LayoutFit, LayoutFill, and LayoutStretch.
 type LayoutFunc = func(ow, oh, vw, vh int) (w, h int)
 
-// LayoutFit is a LayoutFunc that fits the virtual resolution in the outside size.
+// LayoutFit is a LayoutFunc that fits the virtual resolution within the outside size.
 func LayoutFit(ow, oh, vw, vh int) (w, h int) {
 	return vw, vh
 }
 
 // LayoutFill is a LayoutFunc that fills the outside size with the virtual resolution.
+// FIXME: doesn't work right, last I checked
 func LayoutFill(ow, oh, vw, vh int) (w, h int) {
 	if ow > oh {
 		return vw, (vh * ow) / oh
